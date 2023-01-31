9c4c2df11d20a34c86fb70603b7a11a68a7795f5
Ro11ingButler <rollingkai1214@gmail.com>
2023-01-31 13:38:20 +0800

    Add click animation

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -2,17 +2,34 @@ import './styles.scss'
 import { useState, useEffect, useRef, useContext } from 'react'
 import { Skeleton } from '@mui/material'
 import { AppContext } from '../../AppContext'
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import PreviewIcon from '@mui/icons-material/Preview';
 
-export default function MovieCard({ Title, Year, Poster, Type, imdbID }) {
+export default function MovieCard({ Title, Year, Poster, Type, imdbID, state }) {
 
+    const cardRef = useRef()
     const { handleMovieCardClick } = useContext(AppContext)
 
+    const handleClickAnimation = () => {
+        cardRef.current.classList.toggle('clicked');
+        setTimeout(() => {
+            cardRef.current.classList.toggle('clicked');
+        }, 200)
+    }
+
     return (
-        <figure className='movie-card' onClick={() => handleMovieCardClick({ Title, Year, Poster, Type, imdbID })}>
+        <figure ref={cardRef} className='movie-card' onClick={() => {
+            handleMovieCardClick({ Title, Year, Poster, Type, imdbID })
+            handleClickAnimation()
+        }}>
             {Poster !== "N/A" || !Poster ? <img src={Poster} alt="..."></img> : <Skeleton className="skeleton-image" animation={false} variant="rectangular" />}
             <div>
                 <h2>{Title}</h2>
-                <p>{Year}<br></br><span>liked</span></p>
+                <div>
+                    {state === 'favorite' && <ThumbUpIcon fontSize='small'></ThumbUpIcon>}
+                    {state === 'watch-later' && <PreviewIcon fontSize='small'></PreviewIcon>}
+                    <p>{Year}</p>
+                </div>
             </div>
         </figure>
     )
