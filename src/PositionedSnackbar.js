e3c530636870b43c7cfcfbe739261dc6f1037d2a
Ro11ingButler <rollingkai1214@gmail.com>
2023-01-31 22:29:40 +0800

    Fix lint errors

--- src/PositionedSnackbar.js
+++ src/PositionedSnackbar.js
@@ -1,37 +1,34 @@
-import Snackbar from '@mui/material/Snackbar';
-import { useState, useContext, useEffect } from 'react';
-import { AppContext } from './AppContext';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
+import Snackbar from '@mui/material/Snackbar'
+import { useState, useContext, useEffect } from 'react'
+import { AppContext } from './AppContext'
+import IconButton from '@mui/material/IconButton'
+import CloseIcon from '@mui/icons-material/Close'
 
-export default function PositionedSnackbar() {
+export default function PositionedSnackbar () {
+  const { snackBarNotif, setSnackBarNotif } = useContext(AppContext)
 
-    const { snackBarNotif, setSnackBarNotif } = useContext(AppContext)
+  const [state, setState] = useState({
+    open: false,
+    vertical: 'bottom',
+    horizontal: 'center'
+  })
 
-    const [state, setState] = useState({
-        open: false,
-        vertical: 'bottom',
-        horizontal: 'center',
-    });
+  const { vertical, horizontal, open } = state
 
-    const { vertical, horizontal, open } = state;
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return
+    }
 
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
+    setState({ ...state, open: false })
+    setSnackBarNotif('')
+  }
 
-        setState({ ...state, open: false });
-        setSnackBarNotif("")
-    };
+  useEffect(() => {
+    if (snackBarNotif !== '') { setState({ ...state, open: true }) }
+  }, [snackBarNotif])
 
-    useEffect(() => {
-        if (snackBarNotif !== "")
-            setState({ ...state, open: true });
-    }, [snackBarNotif])
-
-
-    return (
+  return (
         <div>
             <Snackbar
                 anchorOrigin={{ vertical, horizontal }}
@@ -52,5 +49,5 @@ export default function PositionedSnackbar() {
                 }
             />
         </div>
-    );
+  )
 }
--- src/components/MoviesGrid/MyCatalogGrid.js
+++ src/components/MoviesGrid/MyCatalogGrid.js
@@ -1,16 +1,15 @@
 import './styles.scss'
 import MovieCard from '../MovieCard'
-import { AppContext } from "../../AppContext"
+import { AppContext } from '../../AppContext'
 import { useContext } from 'react'
 
-export default function MyCatalogGrid() {
+export default function MyCatalogGrid () {
+  const { myCatalog } = useContext(AppContext)
 
-    const { myCatalog } = useContext(AppContext)
-
-    return (
+  return (
         <section className='movies-grid'>
             {myCatalog.length <= 0 && <p className="no-search">No favourites yet</p>}
             {myCatalog && myCatalog.map((movie, key) => <MovieCard key={key} {...movie}></MovieCard>)}
         </section>
-    )
+  )
 }
