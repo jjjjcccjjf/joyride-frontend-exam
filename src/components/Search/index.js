f927523b2ddfa333c7cbfab388dc3c70f37a71be
Ro11ingButler <rollingkai1214@gmail.com>
2023-01-31 23:49:22 +0800

    Improve layout (again)

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -41,23 +41,23 @@ export default function Search () {
   ]
 
   return (
-        <section className="hero-search">
-            <div className='hero-div'>
-                <div className='hero-left'>
-                    <h1>Catalog your favourite shows in seconds.</h1>
-                    <p>Add to your list of <span>favourite</span> shows with a single click. Another click to mark them as <span>watch later</span>, and another to <span>remove</span>.</p>
-                </div>
-                <div className='hero-right'>
-                    {fakeMovies.map((movie, key) => <FakeMovieCard key={key} {...movie}></FakeMovieCard>)}
+    <section className="hero-search">
+      <div className='hero-div'>
+        <div className='hero-left'>
+          <h1>Catalog your favourite shows in seconds.</h1>
+          <p>Add to your list of <span>favourite</span> shows with a single click. Another click to mark them as <span>watch later</span>, and another to <span>remove</span>.</p>
+        </div>
+        <div className='hero-right'>
+          {fakeMovies.map((movie, key) => <FakeMovieCard key={key} {...movie}></FakeMovieCard>)}
 
-                </div>
-            </div>
+        </div>
+      </div>
 
-            <div className='hero-form'>
-                <form onSubmit={e => e.preventDefault()}>
-                    <input type="text" placeholder="Type a show title. (Example: Sherlock Holmes)" name="s" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}></input>
-                </form>
-            </div>
-        </section>
+      <div className='hero-form'>
+        <form onSubmit={e => e.preventDefault()}>
+          <input type="text" placeholder="Type a title (Ex. The Avengers)" name="s" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}></input>
+        </form>
+      </div>
+    </section>
   )
 }
